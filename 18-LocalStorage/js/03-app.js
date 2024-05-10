f266fca0370e9a5d99bdb8decc576d329a1bb83d
ArgueDev <christianarguelloo97@gmail.com>
2024-05-09 19:55:22 -0500

    LocalStorage

---
+++ 18-LocalStorage/js/03-app.js
@@ -0,0 +1,9 @@
+localStorage.removeItem('nombre'); 
+
+/** Actualizar un registro */
+const mesesArray = JSON.parse(localStorage.getItem('meses'));
+console.log(mesesArray);
+mesesArray.push('Nuevo mes');
+console.log(mesesArray);
+
+localStorage.setItem('meses', JSON.stringify(mesesArray));
