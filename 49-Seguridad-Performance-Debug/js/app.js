1977c44a832f4e161994fb1f89f7972082d8569c
ArgueDev <christianarguelloo97@gmail.com>
2024-10-02 21:00:10 -0500

    Debug, Medir Performance

--- 49-Seguridad-Performance-Debug/js/app.js
+++ 49-Seguridad-Performance-Debug/js/app.js
@@ -37,14 +37,30 @@ function consultarCriptomonedas() {
 // llena el select 
 function selectCriptomonedas(criptomonedas) {
 
-    criptomonedas.forEach( cripto => {
-        const { FullName, Name } = cripto.CoinInfo;
+    // Conocer el tiempo de ejecucion
+    const inicio = performance.now();
+
+    // criptomonedas.forEach( cripto => {
+    //     const { FullName, Name } = cripto.CoinInfo;
+    //     const option = document.createElement('option');
+    //     option.value = Name;
+    //     option.textContent = FullName;
+    //     // insertar el HTML
+    //     criptomonedasSelect.appendChild(option);
+    // });
+
+    for (let i = 0; i < criptomonedas.length; i++) {
+        const { FullName, Name } = criptomonedas[i].CoinInfo;
         const option = document.createElement('option');
         option.value = Name;
         option.textContent = FullName;
         // insertar el HTML
         criptomonedasSelect.appendChild(option);
-    });
+    }
+
+    const fin = performance.now();
+
+    console.log(fin - inicio);
 
 }
 
@@ -110,9 +126,6 @@ function mostrarCotizacionHTML(cotizacion) {
     console.log(cotizacion);
     const  { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE } = cotizacion;
 
-
-    debugger;
-
     const precio = document.createElement('p');
     precio.classList.add('precio');
     precio.innerHTML = `El Precio es: <span> ${PRICE} </span>`;
@@ -128,8 +141,6 @@ function mostrarCotizacionHTML(cotizacion) {
 
     const ultimaActualizacion = document.createElement('p');
     ultimaActualizacion.innerHTML = `<p>Última Actualización: <span>${LASTUPDATE}</span></p>`;
-
-    debugger;
 
     resultado.appendChild(precio);
     resultado.appendChild(precioAlto);
